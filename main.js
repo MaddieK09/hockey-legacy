a04f1488a6aa015dc8cf8d8b0bb225df8b1f4fea
MaddieK09 <jakerk0826@gmail.com>
2026-08-02 15:24:01 -0400

    Start main menu

--- main.js
+++ main.js
@@ -37,23 +37,16 @@ function create() {
             fill: "#ffffff"
         }
     ).setOrigin(0.5);
+this.add.text(
+    this.cameras.main.centerX,
+    300,
+    "▶ PLAY",
+    {
+        font: "36px Arial",
+        fill: "#ffff00"
+    }
+).setOrigin(0.5);
 
-    this.add.rectangle(
-        this.cameras.main.centerX,
-        this.cameras.main.centerY,
-        500,
-        900,
-        0xe8f5ff
-    );
 
-    this.add.text(
-        this.cameras.main.centerX,
-        this.cameras.main.centerY,
-        "🏒\nRink Coming Soon",
-        {
-            font: "34px Arial",
-            align: "center",
-            fill: "#000000"
-        }
-    ).setOrigin(0.5);
+
 }

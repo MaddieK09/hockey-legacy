f01bd5f523789c0beab878ab39415e8e9bef6b0a
MaddieK09 <jakerk0826@gmail.com>
2026-08-05 00:04:37 -0400

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ const config = {
     backgroundColor: "#0d7a2b",
 
     scene: {
-        create: create
+        create: create,
+        update: update
     }
 };
 
@@ -29,6 +30,39 @@ function create() {
     rink.top = rink.centerY - rink.height / 2;
     rink.bottom = rink.centerY + rink.height / 2;
 
+    scene.gameState = {
+        rink: rink,
+        gameStarted: false,
+
+        player: null,
+        playerStick: null,
+        puck: null,
+
+        playerVelocityX: 0,
+        playerVelocityY: 0,
+
+        puckVelocityX: 0,
+        puckVelocityY: 0,
+
+        playerSpeed: 155,
+        puckFriction: 0.985,
+
+        playerRadius: 11,
+        puckRadius: 5,
+
+        facingX: 0,
+        facingY: -1,
+
+        controls: {
+            up: false,
+            down: false,
+            left: false,
+            right: false
+        },
+
+        keyboard: null
+    };
+
     const titleText = scene.add.text(
         rink.centerX,
         80,
@@ -42,7 +76,7 @@ function create() {
     const versionText = scene.add.text(
         rink.centerX,
         170,
-        "Version 0.0.38",
+        "Version 0.0.39",
         {
             font: "24px Arial",
             fill: "#ffffff"
@@ -71,7 +105,29 @@ function create() {
         scene.cameras.main.setBackgroundColor("#d8f0ff");
 
         drawRink(scene, rink);
+        createPlayer(scene);
+        createPuck(scene);
+        createMobileControls(scene);
+        createKeyboardControls(scene);
+
+        scene.gameState.gameStarted = true;
     });
+}
+
+function update(time, delta) {
+    const state = this.gameState;
+
+    if (!state || !state.gameStarted) {
+        return;
+    }
+
+    const deltaSeconds = Math.min(delta / 1000, 0.05);
+
+    updatePlayerInput(this);
+    updatePlayerMovement(this, deltaSeconds);
+    updatePuckMovement(this, deltaSeconds);
+    handlePlayerPuckContact(this);
+    updatePlayerStick(this);
 }
 
 function drawRink(scene, rink) {
@@ -85,7 +141,6 @@ function drawRink(scene, rink) {
     drawGoalieTrapezoids(graphics, rink);
     drawRefereeCrease(graphics, rink);
 
-    // Redraw the boards last so markings do not spill outside.
     drawBoardOutline(graphics, rink);
 }
 
@@ -118,7 +173,6 @@ function drawMainLines(graphics, rink) {
     const lineRight = rink.right - 4;
     const blueLineOffset = 150;
 
-    // Center red line
     graphics.lineStyle(4, 0xff3b30, 1);
 
     graphics.lineBetween(
@@ -128,7 +182,6 @@ function drawMainLines(graphics, rink) {
         rink.centerY
     );
 
-    // Blue lines
     graphics.lineStyle(4, 0x1d5fa7, 1);
 
     graphics.lineBetween(
@@ -193,7 +246,6 @@ function drawFaceoffLayout(graphics, rink) {
     drawFaceoffDot(graphics, leftX, bottomY, 3);
     drawFaceoffDot(graphics, rightX, bottomY, 3);
 
-    // Neutral-zone faceoff dots
     drawFaceoffDot(
         graphics,
         rink.centerX - neutralOffsetX,
@@ -235,7 +287,6 @@ function drawCircleHashes(graphics, x, y, radius) {
     const insideLength = 2;
     const hashGap = 5;
 
-    // Top
     graphics.lineBetween(
         x - hashGap,
         y - radius - outsideLength,
@@ -250,7 +301,6 @@ function drawCircleHashes(graphics, x, y, radius) {
         y - radius + insideLength
     );
 
-    // Bottom
     graphics.lineBetween(
         x - hashGap,
         y + radius - insideLength,
@@ -265,7 +315,6 @@ function drawCircleHashes(graphics, x, y, radius) {
         y + radius + outsideLength
     );
 
-    // Left
     graphics.lineBetween(
         x - radius - outsideLength,
         y - hashGap,
@@ -280,7 +329,6 @@ function drawCircleHashes(graphics, x, y, radius) {
         y + hashGap
     );
 
-    // Right
     graphics.lineBetween(
         x + radius - insideLength,
         y - hashGap,
@@ -301,7 +349,6 @@ function drawFaceoffLMarks(graphics, x, y) {
     const verticalOffset = 8;
     const markLength = 6;
 
-    // Top-left L
     graphics.lineBetween(
         x - horizontalOffset,
         y - verticalOffset,
@@ -316,7 +363,6 @@ function drawFaceoffLMarks(graphics, x, y) {
         y - verticalOffset + markLength
     );
 
-    // Top-right L
     graphics.lineBetween(
         x + horizontalOffset,
         y - verticalOffset,
@@ -331,7 +377,6 @@ function drawFaceoffLMarks(graphics, x, y) {
         y - verticalOffset + markLength
     );
 
-    // Bottom-left L
     graphics.lineBetween(
         x - horizontalOffset,
         y + verticalOffset,
@@ -346,7 +391,6 @@ function drawFaceoffLMarks(graphics, x, y) {
         y + verticalOffset - markLength
     );
 
-    // Bottom-right L
     graphics.lineBetween(
         x + horizontalOffset,
         y + verticalOffset,
@@ -480,7 +524,6 @@ function drawGoalCrease(graphics, centerX, goalLineY, side) {
     graphics.fillPath();
     graphics.strokePath();
 
-    // Small crease reference marks
     graphics.lineStyle(2, 0x4fc3ff, 1);
 
     graphics.lineBetween(
@@ -509,10 +552,8 @@ function drawGoalNet(graphics, centerX, goalLineY, side) {
     const firstMeshY = goalLineY + netDepth * 0.33 * direction;
     const secondMeshY = goalLineY + netDepth * 0.66 * direction;
 
-    // Netting
     graphics.lineStyle(1, 0x9fb3c8, 0.95);
 
-    // Outer net shape
     graphics.lineBetween(
         centerX - mouthHalfWidth,
         goalLineY,
@@ -534,7 +575,6 @@ function drawGoalNet(graphics, centerX, goalLineY, side) {
         goalLineY
     );
 
-    // Horizontal mesh
     graphics.lineBetween(
         centerX - 18,
         firstMeshY,
@@ -549,7 +589,6 @@ function drawGoalNet(graphics, centerX, goalLineY, side) {
         secondMeshY
     );
 
-    // Vertical mesh
     graphics.lineBetween(
         centerX - 12,
         goalLineY,
@@ -585,10 +624,8 @@ function drawGoalNet(graphics, centerX, goalLineY, side) {
         backY
     );
 
-    // Red goal frame
     graphics.lineStyle(4, 0xff3b30, 1);
 
-    // Front crossbar
     graphics.lineBetween(
         centerX - mouthHalfWidth,
         goalLineY,
@@ -596,7 +633,6 @@ function drawGoalNet(graphics, centerX, goalLineY, side) {
         goalLineY
     );
 
-    // Left frame
     graphics.lineBetween(
         centerX - mouthHalfWidth,
         goalLineY,
@@ -604,7 +640,6 @@ function drawGoalNet(graphics, centerX, goalLineY, side) {
         backY
     );
 
-    // Right frame
     graphics.lineBetween(
         centerX + mouthHalfWidth,
         goalLineY,
@@ -612,7 +647,6 @@ function drawGoalNet(graphics, centerX, goalLineY, side) {
         backY
     );
 
-    // Back frame
     graphics.lineBetween(
         centerX - backHalfWidth,
         backY,
@@ -632,7 +666,6 @@ function drawGoalieTrapezoids(graphics, rink) {
 
     graphics.lineStyle(2, 0xff3b30, 1);
 
-    // Top trapezoid
     graphics.lineBetween(
         rink.centerX - innerHalfWidth,
         topGoalLineY,
@@ -647,7 +680,6 @@ function drawGoalieTrapezoids(graphics, rink) {
         rink.top + 5
     );
 
-    // Bottom trapezoid
     graphics.lineBetween(
         rink.centerX - innerHalfWidth,
         bottomGoalLineY,
@@ -678,4 +710,548 @@ function drawRefereeCrease(graphics, rink) {
     );
 
     graphics.strokePath();
+}
+
+function createPlayer(scene) {
+    const state = scene.gameState;
+    const rink = state.rink;
+
+    state.player = scene.add.circle(
+        rink.centerX,
+        rink.centerY + 95,
+        state.playerRadius,
+        0x1769d2
+    );
+
+    state.player.setStrokeStyle(
+        3,
+        0xffffff,
+        1
+    );
+
+    state.playerStick = scene.add.graphics();
+}
+
+function createPuck(scene) {
+    const state = scene.gameState;
+    const rink = state.rink;
+
+    state.puck = scene.add.circle(
+        rink.centerX,
+        rink.centerY + 50,
+        state.puckRadius,
+        0x111111
+    );
+
+    state.puck.setStrokeStyle(
+        1,
+        0x555555,
+        1
+    );
+}
+
+function createKeyboardControls(scene) {
+    scene.gameState.keyboard = scene.input.keyboard.addKeys({
+        up: Phaser.Input.Keyboard.KeyCodes.UP,
+        down: Phaser.Input.Keyboard.KeyCodes.DOWN,
+        left: Phaser.Input.Keyboard.KeyCodes.LEFT,
+        right: Phaser.Input.Keyboard.KeyCodes.RIGHT,
+
+        w: Phaser.Input.Keyboard.KeyCodes.W,
+        s: Phaser.Input.Keyboard.KeyCodes.S,
+        a: Phaser.Input.Keyboard.KeyCodes.A,
+        d: Phaser.Input.Keyboard.KeyCodes.D
+    });
+}
+
+function createMobileControls(scene) {
+    const buttonRadius = 27;
+
+    const controlsCenterX = 75;
+    const controlsCenterY = scene.scale.height - 82;
+
+    createControlButton(
+        scene,
+        controlsCenterX,
+        controlsCenterY - 48,
+        "▲",
+        "up",
+        buttonRadius
+    );
+
+    createControlButton(
+        scene,
+        controlsCenterX,
+        controlsCenterY + 48,
+        "▼",
+        "down",
+        buttonRadius
+    );
+
+    createControlButton(
+        scene,
+        controlsCenterX - 48,
+        controlsCenterY,
+        "◀",
+        "left",
+        buttonRadius
+    );
+
+    createControlButton(
+        scene,
+        controlsCenterX + 48,
+        controlsCenterY,
+        "▶",
+        "right",
+        buttonRadius
+    );
+}
+
+function createControlButton(
+    scene,
+    x,
+    y,
+    symbol,
+    controlName,
+    radius
+) {
+    const state = scene.gameState;
+
+    const button = scene.add.circle(
+        x,
+        y,
+        radius,
+        0x17375e,
+        0.72
+    );
+
+    button.setStrokeStyle(
+        2,
+        0xffffff,
+        0.75
+    );
+
+    button.setInteractive({
+        useHandCursor: true
+    });
+
+    const label = scene.add.text(
+        x,
+        y,
+        symbol,
+        {
+            font: "24px Arial",
+            fill: "#ffffff"
+        }
+    ).setOrigin(0.5);
+
+    label.setDepth(button.depth + 1);
+
+    const pressButton = () => {
+        state.controls[controlName] = true;
+
+        button.setFillStyle(
+            0x2f71b7,
+            0.9
+        );
+    };
+
+    const releaseButton = () => {
+        state.controls[controlName] = false;
+
+        button.setFillStyle(
+            0x17375e,
+            0.72
+        );
+    };
+
+    button.on("pointerdown", pressButton);
+    button.on("pointerup", releaseButton);
+    button.on("pointerout", releaseButton);
+    button.on("pointerupoutside", releaseButton);
+}
+
+function updatePlayerInput(scene) {
+    const state = scene.gameState;
+    const keyboard = state.keyboard;
+
+    const keyboardUp =
+        keyboard.up.isDown ||
+        keyboard.w.isDown;
+
+    const keyboardDown =
+        keyboard.down.isDown ||
+        keyboard.s.isDown;
+
+    const keyboardLeft =
+        keyboard.left.isDown ||
+        keyboard.a.isDown;
+
+    const keyboardRight =
+        keyboard.right.isDown ||
+        keyboard.d.isDown;
+
+    let directionX = 0;
+    let directionY = 0;
+
+    if (state.controls.left || keyboardLeft) {
+        directionX -= 1;
+    }
+
+    if (state.controls.right || keyboardRight) {
+        directionX += 1;
+    }
+
+    if (state.controls.up || keyboardUp) {
+        directionY -= 1;
+    }
+
+    if (state.controls.down || keyboardDown) {
+        directionY += 1;
+    }
+
+    if (directionX !== 0 || directionY !== 0) {
+        const length = Math.sqrt(
+            directionX * directionX +
+            directionY * directionY
+        );
+
+        directionX /= length;
+        directionY /= length;
+
+        state.facingX = directionX;
+        state.facingY = directionY;
+    }
+
+    state.playerVelocityX =
+        directionX * state.playerSpeed;
+
+    state.playerVelocityY =
+        directionY * state.playerSpeed;
+}
+
+function updatePlayerMovement(scene, deltaSeconds) {
+    const state = scene.gameState;
+    const player = state.player;
+
+    let nextX =
+        player.x +
+        state.playerVelocityX * deltaSeconds;
+
+    let nextY =
+        player.y +
+        state.playerVelocityY * deltaSeconds;
+
+    const correctedPosition =
+        clampPointInsideRoundedRink(
+            nextX,
+            nextY,
+            state.playerRadius,
+            state.rink
+        );
+
+    player.x = correctedPosition.x;
+    player.y = correctedPosition.y;
+}
+
+function updatePuckMovement(scene, deltaSeconds) {
+    const state = scene.gameState;
+    const puck = state.puck;
+
+    puck.x +=
+        state.puckVelocityX * deltaSeconds;
+
+    puck.y +=
+        state.puckVelocityY * deltaSeconds;
+
+    const correctedPosition =
+        clampPointInsideRoundedRink(
+            puck.x,
+            puck.y,
+            state.puckRadius,
+            state.rink
+        );
+
+    if (correctedPosition.hitX) {
+        state.puckVelocityX *= -0.55;
+    }
+
+    if (correctedPosition.hitY) {
+        state.puckVelocityY *= -0.55;
+    }
+
+    puck.x = correctedPosition.x;
+    puck.y = correctedPosition.y;
+
+    const frameFriction = Math.pow(
+        state.puckFriction,
+        deltaSeconds * 60
+    );
+
+    state.puckVelocityX *= frameFriction;
+    state.puckVelocityY *= frameFriction;
+
+    if (Math.abs(state.puckVelocityX) < 0.5) {
+        state.puckVelocityX = 0;
+    }
+
+    if (Math.abs(state.puckVelocityY) < 0.5) {
+        state.puckVelocityY = 0;
+    }
+}
+
+function handlePlayerPuckContact(scene) {
+    const state = scene.gameState;
+
+    const player = state.player;
+    const puck = state.puck;
+
+    const deltaX = puck.x - player.x;
+    const deltaY = puck.y - player.y;
+
+    const distance = Math.sqrt(
+        deltaX * deltaX +
+        deltaY * deltaY
+    );
+
+    const contactDistance =
+        state.playerRadius +
+        state.puckRadius +
+        4;
+
+    if (distance >= contactDistance) {
+        return;
+    }
+
+    let normalX;
+    let normalY;
+
+    if (distance > 0.001) {
+        normalX = deltaX / distance;
+        normalY = deltaY / distance;
+    } else {
+        normalX = state.facingX;
+        normalY = state.facingY;
+    }
+
+    const overlap = contactDistance - distance;
+
+    puck.x += normalX * overlap;
+    puck.y += normalY * overlap;
+
+    const movementSpeed = Math.sqrt(
+        state.playerVelocityX *
+        state.playerVelocityX +
+        state.playerVelocityY *
+        state.playerVelocityY
+    );
+
+    const pushSpeed =
+        50 +
+        movementSpeed * 0.8;
+
+    state.puckVelocityX =
+        normalX * pushSpeed +
+        state.playerVelocityX * 0.45;
+
+    state.puckVelocityY =
+        normalY * pushSpeed +
+        state.playerVelocityY * 0.45;
+}
+
+function updatePlayerStick(scene) {
+    const state = scene.gameState;
+
+    const player = state.player;
+    const stick = state.playerStick;
+
+    const perpendicularX = -state.facingY;
+    const perpendicularY = state.facingX;
+
+    const handX =
+        player.x +
+        state.facingX * 6 +
+        perpendicularX * 6;
+
+    const handY =
+        player.y +
+        state.facingY * 6 +
+        perpendicularY * 6;
+
+    const bladeX =
+        player.x +
+        state.facingX * 25 +
+        perpendicularX * 8;
+
+    const bladeY =
+        player.y +
+        state.facingY * 25 +
+        perpendicularY * 8;
+
+    stick.clear();
+
+    stick.lineStyle(
+        3,
+        0x6e4524,
+        1
+    );
+
+    stick.lineBetween(
+        handX,
+        handY,
+        bladeX,
+        bladeY
+    );
+
+    stick.lineStyle(
+        4,
+        0x222222,
+        1
+    );
+
+    stick.lineBetween(
+        bladeX,
+        bladeY,
+        bladeX + perpendicularX * 8,
+        bladeY + perpendicularY * 8
+    );
+}
+
+function clampPointInsideRoundedRink(
+    x,
+    y,
+    objectRadius,
+    rink
+) {
+    const insetLeft =
+        rink.left + objectRadius + 4;
+
+    const insetRight =
+        rink.right - objectRadius - 4;
+
+    const insetTop =
+        rink.top + objectRadius + 4;
+
+    const insetBottom =
+        rink.bottom - objectRadius - 4;
+
+    const innerCornerRadius =
+        Math.max(
+            rink.cornerRadius -
+            objectRadius -
+            4,
+            1
+        );
+
+    let correctedX = Phaser.Math.Clamp(
+        x,
+        insetLeft,
+        insetRight
+    );
+
+    let correctedY = Phaser.Math.Clamp(
+        y,
+        insetTop,
+        insetBottom
+    );
+
+    let hitX = correctedX !== x;
+    let hitY = correctedY !== y;
+
+    const cornerCenters = [
+        {
+            x: rink.left + rink.cornerRadius,
+            y: rink.top + rink.cornerRadius
+        },
+        {
+            x: rink.right - rink.cornerRadius,
+            y: rink.top + rink.cornerRadius
+        },
+        {
+            x: rink.left + rink.cornerRadius,
+            y: rink.bottom - rink.cornerRadius
+        },
+        {
+            x: rink.right - rink.cornerRadius,
+            y: rink.bottom - rink.cornerRadius
+        }
+    ];
+
+    const isLeftSide =
+        correctedX <
+        rink.left + rink.cornerRadius;
+
+    const isRightSide =
+        correctedX >
+        rink.right - rink.cornerRadius;
+
+    const isTopSide =
+        correctedY <
+        rink.top + rink.cornerRadius;
+
+    const isBottomSide =
+        correctedY >
+        rink.bottom - rink.cornerRadius;
+
+    let cornerCenter = null;
+
+    if (isLeftSide && isTopSide) {
+        cornerCenter = cornerCenters[0];
+    } else if (isRightSide && isTopSide) {
+        cornerCenter = cornerCenters[1];
+    } else if (isLeftSide && isBottomSide) {
+        cornerCenter = cornerCenters[2];
+    } else if (isRightSide && isBottomSide) {
+        cornerCenter = cornerCenters[3];
+    }
+
+    if (cornerCenter) {
+        const deltaX =
+            correctedX - cornerCenter.x;
+
+        const deltaY =
+            correctedY - cornerCenter.y;
+
+        const distance = Math.sqrt(
+            deltaX * deltaX +
+            deltaY * deltaY
+        );
+
+        if (
+            distance >
+            innerCornerRadius
+        ) {
+            const normalX =
+                deltaX / distance;
+
+            const normalY =
+                deltaY / distance;
+
+            const newX =
+                cornerCenter.x +
+                normalX * innerCornerRadius;
+
+            const newY =
+                cornerCenter.y +
+                normalY * innerCornerRadius;
+
+            hitX =
+                hitX ||
+                Math.abs(newX - correctedX) > 0.01;
+
+            hitY =
+                hitY ||
+                Math.abs(newY - correctedY) > 0.01;
+
+            correctedX = newX;
+            correctedY = newY;
+        }
+    }
+
+    return {
+        x: correctedX,
+        y: correctedY,
+        hitX: hitX,
+        hitY: hitY
+    };
 }
